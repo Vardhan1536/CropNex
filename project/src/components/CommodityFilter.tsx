d15a035c6617f72edd4d04a16d4e5fcd0b3a87f0
Vardhan1536 <balavardhantummalcherla@gmail.com>
2025-06-18 22:48:03 +0530

    fixed

--- project/src/components/CommodityFilter.tsx
+++ project/src/components/CommodityFilter.tsx
@@ -80,7 +80,7 @@ const CommodityFilter = () => {
     }
 
     try {
-      const response = await fetch(`http://127.0.0.1:8000/predict`, {
+      const response = await fetch(`https://cropnex.onrender.com/predict`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
